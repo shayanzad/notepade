e1cb6a5578526d856d7172a51cdccb8a2ca94e18
shayan azadbakht <shayanazadbakht1781@gmail.com>
2021-08-03 23:28:33 +0430

    add a event listener

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+eventlestener();
+
+function eventlestener() {
+    document.querySelector("#form").addEventListener("submit", additemlist);
+}
+
+function additemlist(e) {
+    e.preventDefault();
+
+
+}
